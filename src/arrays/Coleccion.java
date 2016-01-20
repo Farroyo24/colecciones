752b553c00f4f758317ee6a29d3a787e3362508f
Fran Arroyo <francisco.arroyoeliche@gmail.com>
2016-01-20 15:38:51 +0100

    Miercoles 20 Enero

--- src/arrays/Coleccion.java
+++ src/arrays/Coleccion.java
@@ -1,7 +1,11 @@
-package arrays;
 
+package arrays;
+import java.util.Arrays;
 public class Coleccion {
-	static double[][] vector;
+	private double[][] vector;
+	//private double[][] vector???
+	//constructor inicializa vector como una matriz
+	//de filas y columnas (filas X columnas)
 	
 	public Coleccion(int fila, int columna){
 		this.vector=new double[fila][columna];
@@ -10,18 +14,97 @@ public class Coleccion {
 	public Coleccion(double[][] vector) {
 		this.vector = vector;
 	}
-	public static void llenarColeccion(int numeroMultiplicador){
+	//metodo que rellena el array de forma aleatoria
+	//usamos Math.random() y un multiplicador
+	public void llenarColeccion(int numeroMultiplicador){
 		for (int i = 0; i <vector.length; i++){
 			for(int j=0;j<vector[i].length;j++){
-		//		double numeroAlmacenar = Math.random()*numeroMultiplicador;
 				vector[i][j]=Math.random()*numeroMultiplicador;
-			//	numeroAlmacenar*=numeroMultiplicador;		}
+			}
+	
 		}
 	}
-
+	public void mostrarColeccion(){
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j<vector[i].length; j++) {
+				System.out.printf("%.1f ", vector[i][j]);
+			}
+			System.out.println();//salto de linea;
+		}
+	}
+	public double mostrarMenor(){
+		double menor=vector[0][0];
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j<vector[i].length; j++) {
+				if(vector[i][j]<menor){
+					menor=vector[i][j];
+				}
+			}
+		}
+		return menor;
+	}
+	public double mostrarMayor(){
+		double mayor=vector[0][0];
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j<vector[i].length; j++) {
+				if(vector[i][j]>mayor){
+					mayor=vector[i][j];
+				}
+			}
+		}
+		return mayor;
+	}
+	public double mostrarMedia(){
+		double suma=0;
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j<vector[i].length; j++) {
+				suma+=vector[i][j];
+			}
+		}
+		double media=suma/(vector.length*vector[0].length);
+		return media;
+	}
+	/*public static void sort(double[] a){
+		double[] arr={0,1,2,3,4};
+		double[] arr2={0,5,10};
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j<arr2.length; j++) {
+				System.out.printf
+			}
+		}
+	}*/
+	public void convertirArray(){
+		double[] arrayConvertido=new double[vector.length*vector[0].length];
+		int contador=0;
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j<vector[i].length; j++) {
+				//arrayConvertido=
+			}
+		}
+	}
+	public void calcularValoresEstadisticos(){
+		int tamano=vector.length*vector[0].length;
+		double[] vectorUnidimensional=new double[tamano];
+		int contador=0;
+		for(double[] ds : vector){
+			for(double d : ds){
+				vectorUnidimensional[contador]=d;
+				contador++;
+			}
+		}
+		Arrays.sort(vectorUnidimensional);
+		System.out.printf("Valor mas grande: %.2f  y el mas pequeño %.2f%n", vectorUnidimensional[vectorUnidimensional.length-1], vectorUnidimensional[0]);
+	}
 	@Override
-	public String toString() {
-		return "";
+	public String toString(){
+		String vectorCadena="";
+		//logica
+		for (int i = 0; i < vector.length; i++) {
+			for (int j = 0; j<vector[i].length; j++) {
+				vectorCadena+=this.vector[i][j]+"-";
+			}
+		}
+		return vectorCadena.substring(0,vectorCadena.length()-1);
 	}
 	
 	
--- src/arrays/TestColeccion.java
+++ src/arrays/TestColeccion.java
@@ -3,8 +3,18 @@ package arrays;
 public class TestColeccion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Coleccion c=new Coleccion(3,4);
+		c.llenarColeccion(10);
+		c.mostrarColeccion();
+		System.out.printf("Numero menor: %.1f%n", c.mostrarMenor());
+		System.out.printf("Numero mayor: %.1f%n", c.mostrarMayor());
+		System.out.printf("Media: %.1f%n", c.mostrarMedia());
+		/*double[][] c2=new Coleccion{{1,2,3},{4,5,6}};
 
+		double[][] valores=c.getVector();*/
+		System.out.println(c);
+		c.calcularValoresEstadisticos();
+		
 	}
 
 }
